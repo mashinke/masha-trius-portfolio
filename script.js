832f491c520642e034aa57f30b9d56d7e0685492
Masha Trius <triusmaria@gmail.com>
2021-01-21 13:48:28 -0600

    improve project styles

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function setContentBottom(currentWindow) {
-  let content = document.getElementsByClassName('content')[0];
-  content.style.bottom = (window.innerHeight - content.offsetHeight) + 'px';
+  setTimeout(function () {
+    let content = document.getElementsByClassName('content')[0];
+    content.style.bottom = (window.innerHeight - content.offsetHeight) + 'px';
+  }, 200);
 }
 
 function main() {
   setContentBottom(window);
 
   window.addEventListener("orientationchange", function (event) {
-    setTimeout(function () {
-      setContentBottom(event.target);
-    }, 200)
+    setContentBottom(event.target);
   });
 }
 
